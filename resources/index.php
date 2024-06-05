6f695b50fec3273527f6230affab0b5997aed719
comers-zen <comers.zen_0d@icloud.com>
2024-06-05 16:21:32 +0300

    Updated resources

--- resources/index.php
+++ resources/index.php
@@ -3,3 +3,4 @@ Serverless Pull Request Blockchain Lambda Functions Agile Security
 Dream big and dare to fail. Difficult roads often lead to beautiful destinations. Success doesn’t just find you. You have to go out and get it. Deployment Kubernetes
 Success is not in what you have, but who you are. Innovation distinguishes between a leader and a follower. Load Testing Optimization Replica Set Deployment Logging Event-driven Architecture Opportunities don't happen. You create them. Security
 JavaScript Act as if what you do makes a difference. It does. DevOps Security Database Indexing Kafka Framework Replica Set Stay hungry, stay foolish. Agile
+Testing WebSockets Load Balancing Work hard in silence, let your success be your noise. Database Indexing
