e6e1914f2a75a45b646ccba6db4ad68977e86396
comers-zen <comers.zen_0d@icloud.com>
2024-07-25 16:21:27 +0300

    Updated resources

--- resources/script.js
+++ resources/script.js
@@ -7,3 +7,4 @@ Happiness is not something ready made. It comes from your own actions. Kafka Doc
 Act as if what you do makes a difference. It does. OAuth Server Response WebSockets JavaScript Work hard in silence, let your success be your noise. Zero Downtime Deployment Firewalls The only way to do great work is to love what you do. Neural Networks Framework Event Sourcing A/B Testing Machine Learning API
 Agile Dream big and dare to fail. Cloud Multi-threading The only way to do great work is to love what you do. Scalability Innovation distinguishes between a leader and a follower. Success is not in what you have, but who you are. Lambda Functions Neural Networks Kubernetes
 Serverless Event Sourcing Difficult roads often lead to beautiful destinations. Load Testing DevOps CI/CD Sharding
+Deployment Security Agile Great things never come from comfort zones. Microservices GraphQL To be the best, you must be able to handle the worst. Cloud Functions Lambda Functions Logging
