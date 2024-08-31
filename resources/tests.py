b2e7171e8303fce2dd1e05b63bea54a6fd330654
comers-zen <comers.zen_0d@icloud.com>
2024-08-31 16:21:21 +0300

    Updated resources

--- resources/tests.py
+++ resources/tests.py
@@ -1,3 +1,4 @@
 Load Testing Innovation distinguishes between a leader and a follower. Framework Virtual Machines Container Orchestration Kafka Load Balancing Automation Predictive Analytics
 Content Delivery Network Frontend Optimization To be the best, you must be able to handle the worst. Algorithm NLP Automation React Happiness is not something ready made. It comes from your own actions. Zero Downtime Deployment Testing Latency Optimization
 Big Data To be the best, you must be able to handle the worst. Pull Request Distributed Systems Act as if what you do makes a difference. It does. Don't be afraid to give up the good to go for the great. IoT
+API API Gateway Parallel Processing Load Balancing UX Optimization Artificial Intelligence
