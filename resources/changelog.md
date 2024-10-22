3120b50d5556e1163a5b908e736b37e466ab68f0
comers-zen <comers.zen_0d@icloud.com>
2024-10-22 16:21:15 +0300

    Updated resources

--- resources/changelog.md
+++ resources/changelog.md
@@ -1,1 +1,2 @@
 Deployment To be the best, you must be able to handle the worst. Opportunities don't happen. You create them. Cloud NLP Microservices Machine Learning Backend A/B Testing Kubernetes DevOps Security DNS Routing RabbitMQ
+Cache Invalidation Server Response Multi-threading Simplicity is the ultimate sophistication. Parallel Processing Serverless
--- resources/setup.sh
+++ resources/setup.sh
@@ -2,3 +2,4 @@ Frontend Event Sourcing Content Delivery Network Load Balancing Latency Optimiza
 Version Control JavaScript Code Review Message Queue Framework Cache Invalidation OAuth Security
 Serverless Load Balancing Database Work hard in silence, let your success be your noise. Parallel Processing
 Docker Work hard in silence, let your success be your noise. Performance The only way to do great work is to love what you do. Success usually comes to those who are too busy to be looking for it. Cache Invalidation UX Optimization Load Testing Big Data
+Deployment CI/CD Kafka Innovation distinguishes between a leader and a follower. Frontend Simplicity is the ultimate sophistication. Load Balancing A/B Testing Multi-threading GraphQL Replica Set Artificial Intelligence Happiness is not something ready made. It comes from your own actions. Server Response
